d33dd700d030a21d6b506cdc4f74259053fb53ef
liulixu <hp_cdxy_liulixu@163.com>
2019-04-02 13:34:51 +0800

    修改后的Toast

--- easyp/src/main/java/com/liu/easyp/LiuPermissions.java
+++ easyp/src/main/java/com/liu/easyp/LiuPermissions.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class LiuPermissions {
-    private static Toast t;
-    public static void show(Context context,String string){
-        if (t == null){
-            t = Toast.makeText(context,string,Toast.LENGTH_SHORT);
-        }else {
-            t.setText(string);
-        }
-        t.show();
+    private static Toast toast;
+    public static void show(Context context, String str){
+        /**
+         * 为防止内存溢出，不直接使用context。此处改为getApplicationContext()
+         */
+        if (toast == null){
+            toast = Toast.makeText(context.getApplicationContext(),str,Toast.LENGTH_SHORT);
+        }else toast.setText(str);
+        toast.show();
     }
 }
