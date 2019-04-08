6becccbd36eb5e1ea43ccfbe1d5b292e5326af24
liulixu <hp_cdxy_liulixu@163.com>
2019-04-08 09:52:38 +0800

    add provider xml

--- app/src/main/java/com/liu/myapplication/MainActivity.java
+++ app/src/main/java/com/liu/myapplication/MainActivity.java
@@ -18,17 +18,18 @@ public class MainActivity extends AppCompatActivity {
     private Button mBtnShowDialog;
     private ImageView mImgView;
 
-    private String picturePath=null;
+    private String picturePath = null;
     TakePhotoes takePhotoes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LiuPermissions.show(this,"123");
+        LiuPermissions.show(this, "123");
         mBtnShowDialog = findViewById(R.id.show_dialog);
         mImgView = findViewById(R.id.img_);
-        takePhotoes = new TakePhotoes(this,picturePath);
-        takePhotoes.recoverState(savedInstanceState,MainActivity.class);
+        takePhotoes = new TakePhotoes(this, picturePath);
+        takePhotoes.recoverState(savedInstanceState, MainActivity.class);
         mBtnShowDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,12 +41,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case C.GALLERY_CODE :
-                if (resultCode == Activity.RESULT_OK){
+        switch (requestCode) {
+            case C.GALLERY_CODE:
+                if (resultCode == Activity.RESULT_OK) {
                     Uri uri = data.getData();
-
+                    takePhotoes.loadGalleryBitmap(uri, mImgView);
                 }
+                break;
+            case C.PICTURE_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+                    takePhotoes.loadPictureBitmap(mImgView);
+                }
+            default:
+                break;
         }
     }
 }
